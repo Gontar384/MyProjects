4cb620075cdfe18b99f2b1e9d72e6518173b40ee
Gontar384 <gontarek384@gmail.com>
2024-08-05 01:32:24 +0200

    Calculator1

---
+++ Calculator/src/main/java/base/ControlPanel.java
@@ -0,0 +1,86 @@
+package base;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ControlPanel {
+
+    public static void calculator() {
+
+        Scanner sc = new Scanner(System.in);
+
+        char userChoice;
+        double x = 0;
+        double y;
+
+        Display.opener();
+
+        while (true) {
+
+            try {
+                x = sc.nextDouble();
+                Display.diplayer(x);
+            } catch (InputMismatchException e) {
+                if (sc.next().equalsIgnoreCase("E")) {
+                    System.exit(0);
+                }
+                System.out.println("If you want to leave, press E");
+            }
+
+            while (true) {
+                if (x == 0) {
+                    System.out.println(Display.conv(x));
+                    break;
+                }
+
+                try {
+                    userChoice = sc.next().toUpperCase().charAt(0);
+                    switch (userChoice) {
+                        case '+' -> {
+                            y = sc.nextDouble();
+                            x = x + y;
+                            Display.diplayer(x);
+                        }
+                        case '-' -> {
+                            y = sc.nextDouble();
+                            x = x - y;
+                            Display.diplayer(x);
+                        }
+                        case 'X' -> {
+                            y = sc.nextDouble();
+                            x = x * y;
+                            Display.diplayer(x);
+                        }
+                        case '/' -> {
+                            y = sc.nextDouble();
+                            if (y == 0){
+                                System.out.println("You cannot divide by zero!");
+                                Display.diplayer(x);
+                                break;
+                            }
+                            x = x / y;
+                            Display.diplayer(x);
+                        }
+                        case '>' -> {
+                            x = x * (-1);
+                            Display.diplayer(x);
+                        }
+                        case '%' -> {
+                            double z = x * 100;
+                            Display.diplayer(z);
+                        }
+                        case 'C' -> x = 0;
+                        case 'E' -> {
+                            System.out.println("Leaving..");
+                            sc.close();
+                            System.exit(0);
+                        }
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Use numbers!");
+                    Display.diplayer(x);
+                }
+            }
+        }
+    }
+}
