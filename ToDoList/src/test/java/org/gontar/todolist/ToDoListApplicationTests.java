1f854099c4fc493cd44fa3bfb35c565ef9abfbfd
Gontar384 <gontarek384@gmail.com>
2024-09-01 18:12:21 +0200

    final

--- ToDoList/src/test/java/org/gontar/todolist/ToDoListApplicationTests.java
+++ ToDoList/src/test/java/org/gontar/todolist/ToDoListApplicationTests.java
@@ -1,13 +1,8 @@
 package org.gontar.todolist;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToDoListApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
