1d6676dcbfcc6fb47fec4ea4ff2d32364a372a18
Gontar384 <gontarek384@gmail.com>
2024-10-18 22:12:45 +0200

    TaskManagerFullApp #complete

--- ToDoListAppv2/frontend/vite.config.js
+++ ToDoListAppv2/frontend/vite.config.js
@@ -1,20 +1,6 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-        plugins: [react()],
-        build: {
-            outDir: 'dist',
-        },
-        base: "/",
-        server: {
-            proxy: {
-                '/api': {
-                    target: import.meta.env.VITE_BACKEND_URL,
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(/^\/api/, '')
-                }
-            }
-        }
-    }
-);
+    plugins: [react()],
+})
